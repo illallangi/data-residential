aaf0c25b21f3355b95f34cf79f1c2f9f4593d799
Andrew Cole <andrew.cole@illallangi.com>
2024-10-23 23:20:04 +1100

    feat(illallangi-django-data): updated to use django.models for model list

--- src/illallangi/data/residential/apps.py
+++ src/illallangi/data/residential/apps.py
@@ -1,42 +1,55 @@
 from django.apps import AppConfig
 from django.conf import settings
+from django.db.models.signals import post_migrate
 
-from illallangi.django.data.models.models_manager import ModelsManager
 from illallangi.rdf.adapters import ResidentialAdapter as RDFAdapter
+
+
+def add_model(
+    **_kwargs: dict[str, object],
+) -> None:
+    from illallangi.django.data.models import Model, Synchronize
+
+    Model.objects.update_or_create(
+        description="Not just a location on a map, but a place where dreams are nurtured and memories are made.",
+        icon="residential/residences.jpg",
+        model="illallangi.data.residential.models.Residence",
+        plural="Residences",
+        singular="Residence",
+        url="residences_html",
+    )
+
+    Synchronize.objects.update_or_create(
+        callable="illallangi.data.residential.apps.synchronize",
+    )
 
 
 class ResidentialHistoryConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "illallangi.data.residential"
 
-    def ready(self) -> None:
-        ModelsManager().add_model(
-            singular="Residence",
-            plural="Residences",
-            model="illallangi.data.residential.models.Residence",
-            url="residences_html",
-            icon="residential/residences.jpg",
-            description="Not just a location on a map, but a place where dreams are nurtured and memories are made.",
+    def ready(
+        self,
+    ) -> None:
+        post_migrate.connect(
+            add_model,
+            sender=self,
         )
 
-        def synchronize() -> None:
-            from illallangi.data.residential.adapters import (
-                ResidentialAdapter as DjangoAdapter,
-            )
 
-            src = RDFAdapter(
-                **settings.RDF,
-            )
-            dst = DjangoAdapter()
+def synchronize() -> None:
+    from illallangi.data.residential.adapters import (
+        ResidentialAdapter as DjangoAdapter,
+    )
 
-            src.load(
-                **settings.RESIDENTIAL,
-            )
-            dst.load()
+    src = RDFAdapter(
+        **settings.RDF,
+    )
+    dst = DjangoAdapter()
 
-            src.sync_to(dst)
+    src.load(
+        **settings.RESIDENTIAL,
+    )
+    dst.load()
 
-        ModelsManager().add_synchronize(
-            name=__name__,
-            synchronize=synchronize,
-        )
+    src.sync_to(dst)
